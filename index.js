252add6a7a891fe23f80090cc3e25b225e1aa881
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-23 10:00:05 +0100

    Added index.js and bookmark class toggle

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector(".bookmark");
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
