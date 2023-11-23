29f94aefe5b969897d962e3978376ade1e57897e
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-23 10:57:52 +0100

    Implemented toggles for bookmarks and answers

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const bookmarkButton = document.querySelector(".bookmark");
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
 });
+
+const answerButton = document.querySelector(".card__button-answer");
+const answer = document.querySelector("p");
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+});
